4ba6e73fcc23c55caaff36047bcd31b556e38e2e
Jpisnice <108455222+Jpisnice@users.noreply.github.com>
2022-10-01 23:44:57 -0700

    halfway there check function yet to be done

---
+++ IT/thirdyear-DSA/expt2/PostToInfix.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#define MAX 5
+
+using namespace std;
+
+class stack1
+{
+private:
+    int stackTop,expTop;
+    string stack[MAX],expression;
+
+public:
+    stack1()
+    {
+     stackTop = -1;
+        for (int i = 0; i < MAX; i++)
+        {
+            stack[i] = " ";
+        }
+    }
+    string pop();
+    void push(string expr);
+    void seek();
+    void display();
+    bool isempty();
+    bool isfull();
+    string combo(string optr1,char optr2,char opr);
+    void getexp();
+};
+
+//-----------Gets Expression From User------------------------------------------
+void stack1::getexp()
+{
+    cout<<"Enter the Postfix Expression"<<endl;
+    cin>>expression;
+    expTop=expression.length()-1;
+    
+}
+
+/*
+-------------------------------------------------------------------
+    Inputs:
+    takes 2 values from the stack which will be operands
+    either as single characters or whole strings,these
+    values will be passed as optr1 and opttr2,it will
+    also take operators such as +,-,* etc.. as char.
+    these will be passed in place of opr.
+
+    working:
+    combines all the passed values into a single string
+    into the following format
+
+    ( optr1 opr optr2 )
+
+    and finaly returns this string
+----------------------------------------------------------------------
+*/
+
+string stack1::combo(string optr1, char optr2, char opr)
+{
+    string expr;
+    
+    expr="("+optr1+opr+optr2+")";
+    return expr;
+}
+
+/*
+------------------------------------------------------------------------
+    Working:
+    pops the top value from the stack
+    and returns it.
+    decrements the top pointer
+    and initializes the poped element to " "
+-------------------------------------------------------------------------
+*/
+string stack1 ::pop()
+{
+    if (isempty())
+    {
+        cout << endl
+             << "The stack1 is empty" << endl;
+        return;
+    }
+    return stack [stackTop];
+    stack [stackTop] = " ";
+ stackTop--;
+}
+
+void stack1 ::push(string expr)
+{
+    if (isfull())
+    {
+        cout << endl
+             << "The stack1 is full" << endl;
+        return;
+    }
+ stackTop++;
+    stack [stackTop] = expr;
+}
+
+bool stack1 ::isempty()
+{
+    if  (stackTop == -1)
+        return true;
+    else
+        return false;
+}
+
+bool stack1 ::isfull()
+{
+    if  (stackTop == MAX - 1)
+        return true;
+    else
+        return false;
+}
